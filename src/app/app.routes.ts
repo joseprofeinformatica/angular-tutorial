ea2ed50726520b9931df884faf4a78b6de265725
Jose Antonio <profesorjoseantoniorodriguez@gmail.com>
2025-11-30 20:37:02 +0100

    Limpieza proyecto

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { TasklistComponent } from './components/task/tasklist/tasklist.component';
-import { TaskresumeComponent } from './components/task/taskresume/taskresume.component';
-
-export const routes: Routes = [
-    {path:'home', component:TaskresumeComponent},
-    {path:'list', component:TasklistComponent},
-    {path:'',redirectTo:'/home',pathMatch:'full'}];
